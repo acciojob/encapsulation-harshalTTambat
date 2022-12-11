70195a490697b2bf42aeb974fdfc2926e159a61a
Harshal Tambat <harshaltambat38@gmail.com>
2022-12-11 17:46:00 +0530

    test

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String []args)
+  {
+      RWOnly obj = new RWOnly();
+      /*
+      obj.name = "Harshal";
+      System.out.println(obj.name);
+      error
+      java: name has private access in com.driver.RWOnly
+      * */
+      obj.setName("Harshal Tambat");
+      System.out.println(obj.getName());
+  }
 }
